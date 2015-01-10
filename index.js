9a7735765fb9fe456fbd1b97410c40b411695cf1
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2015-01-10 10:28:48 -0500

    add: rate limiting

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ var Promise = require('bluebird'),
     request = require('request');
 
 module.exports = function(options) {
-  var tokens = {};
+  var tokens = {},
+      limitSecond,
+      limitCount = 0;
+
+  options = options || {};
+
+  function limit() {
+    var time = (new Date()).valueOf(),
+        second = Math.floor(time/1000);
+    
+    if (second !== limitSecond) {
+      limitSecond = second;
+      limitCount = 0;
+    }
+    limitCount += 1;
+    return (limitCount > (options.rateLimit || 20))
+      ? Promise.delay(time-second*1000).then(limit) 
+      : Promise.fulfilled();
+  }
+
 
   function getToken(email) {
     if (!tokens[email]) {
@@ -51,26 +70,30 @@ module.exports = function(options) {
           options.headers = options.headers || {};
           options.headers.Authorization = [jwt.credentials.token_type,jwt.credentials.access_token].join(' ');
           if (options.gzip === undefined) options.gzip = true;
-          return new Promise(function(resolve,reject) {
-            request(options,function(err,res,d) {
-              if (err) return reject(err);
-              if (d.error) return reject(d.error);
-              else resolve(d);
+  
+          return limit()
+            .then(function() {
+              return new Promise(function(resolve,reject) {
+                request(options,function(err,res,d) {
+                  if (err) return reject(err);
+                  if (d.error) return reject(d.error);
+                  else resolve(d);
+                });
+              })
+              .catch(function(e) {
+                if (retry || e.message !== 'Invalid Credentials') throw e;
+                var token = jwt.credentials.access_token;
+                tokens[email] = getToken(email)
+                  .then(function(jwt) {
+                    if (jwt.credentials.access_token !== token) return jwt;
+                    return jwt.refresh();
+                  });
+                return tokens[email]
+                  .then(function() {
+                    return jwt.request(options,null,true);
+                  });
+              });
             });
-          })
-          .catch(function(e) {
-            if (retry) throw e;
-            var token = jwt.credentials.access_token;
-            tokens[email] = getToken(email)
-              .then(function(jwt) {
-                if (jwt.credentials.access_token !== token) return jwt;
-                return jwt.refresh();
-              });
-            return tokens[email]
-              .then(function() {
-                return jwt.request(options,null,true);
-              });
-          });
         };
 
         jwt.authorize(function(err) {
